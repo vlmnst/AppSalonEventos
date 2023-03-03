126571e704c4416444d2c6d33833d982d54a53a4
vlmnst <vale-mansueto@hotmail.com>
2023-03-03 15:55:16 -0300

    change cors

--- api/index.js
+++ api/index.js
@@ -10,18 +10,24 @@ app.use(express.json());
 dotenv.config();
 conectarDB();
 
-const dominiosPermitidos = [`${process.env.CLIENT_URL}`]
-const corsOptions = {
-    origin: function ( origin, callback ) {
-        if(dominiosPermitidos.indexOf(origin) !== -1){
-            callback(null, true);
-        } else {
-            callback(new Error("No permitido por CORS"))
-        }
-    },
-};
+// const dominiosPermitidos = [`${process.env.CLIENT_URL}`]
+// const corsOptions = {
+//     origin: function ( origin, callback ) {
+//         if(dominiosPermitidos.indexOf(origin) !== -1){
+//             callback(null, true);
+//         } else {
+//             callback(new Error("No permitido por CORS"))
+//         }
+//     },
+// };
 
-// app.use(cors(corsOptions));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+  });
 
 app.use('/eventPlanner', plannerRoutes);
 app.use('/client', clientRoutes);
